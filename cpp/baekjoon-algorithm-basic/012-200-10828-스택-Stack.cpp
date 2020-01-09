0e6e0ee58034930ec817789ee71d12c8500bebb5
theorist17 <theorist17@gmail.com>
2020-01-09 13:38:13 +0900

    15 min. implement stack.

---
+++ cpp/baekjoon-algorithm-basic/012-200-10828-스택-Stack.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Stack
+{
+	int A[10000], m;
+
+public:
+	Stack() { m = 0; }
+	void push(int n){
+		if (m == 100000)
+			return;
+		A[m] = n;
+		m += 1;
+	}
+	int pop() {
+		if (empty())
+			return -1;
+		int ret = A[m-1];
+		m -= 1;
+		return ret;
+	}
+	int top() {
+		if (empty())
+			return -1;
+		return A[m-1];
+	}
+	int empty() {
+		if (m == 0)
+			return 1;
+		else 
+			return 0;
+	}
+	int size() {
+		return m;
+	}
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	Stack s;
+
+	int N;
+	cin >> N;
+
+	for (int i = 0 ; i < N ; i++){
+		string str;
+		cin >> str;
+		
+		if (!str.compare("push")){
+			int op;
+			cin >> op;
+			cin.ignore();
+			s.push(op);
+		} else if (!str.compare("pop")){
+			cout << s.pop() << '\n';
+		} else if (!str.compare("top")){
+			cout << s.top() << '\n';
+		} else if (!str.compare("empty")){
+			cout << s.empty() << '\n';
+		} else if (!str.compare("size")){
+			cout << s.size() << '\n';
+		}
+	}
+	return 0;
+}
