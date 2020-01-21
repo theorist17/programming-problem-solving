a0514cccd6e4479bd71a7260540e7cc01552c6eb
theorist17 <theorist17@gmail.com>
2020-01-21 17:02:55 +0900

    1 min.

---
+++ cpp/baekjoon-algorithm-basic/014-400-11727-2xn타일링2-DP-S3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int D[1001];
+    D[0] = 1;
+    D[1] = 1;
+
+    for(int i = 2; i <= n; i++)
+    {
+        D[i] = (D[i-1] + 2*D[i-2]) % 10007;
+    }
+    cout << D[n] << '\n';
+
+	return 0;
+}
