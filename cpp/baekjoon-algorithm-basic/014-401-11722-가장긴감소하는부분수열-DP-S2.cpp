720d7bf7b885b557f814dc001880a087e416d4e9
theorist <theorist17@gmail.com>
2020-01-25 04:00:06 +0900

    20 min.

--- cpp/baekjoon-algorithm-basic/014-401-11722-가장긴감소하는부분수열-DP-S2.cpp
+++ cpp/baekjoon-algorithm-basic/014-401-11722-가장긴감소하는부분수열-DP-S2.cpp
@@ -7,16 +7,27 @@ int main()
     int n;
     cin >> n;
 
-    vector<int> A(n+1), D(n+1, 1);
+    vector<int> A(n+1), D(n+1, 1), P(n+1, 1);
 
     for (int i = 1; i <= n ; i++)
     {
         cin >> A[i];
         for (int j = i; j >= 1; j--)
         {
-            if (A[j] > A[i] && D[j] + 1 > D[i])
+            if (A[j] < A[i] && D[j] + 1 > D[i])
             {
                 D[i] = D[j] + 1;
+            }
+        }
+    }
+
+    for (int i = n; i >= 1 ; i--)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            if (A[i] > A[j] && P[j] + 1 > P[i])
+            {
+                P[i] = P[j] + 1;
             }
         }
     }
@@ -24,11 +35,10 @@ int main()
     int maxv = 0;
     for (int i = 1 ; i <= n ; i++)
     {
-        //cout << D[i] << ' ';
-        if(maxv < D[i])
-            maxv = D[i];
+        if(maxv < P[i] + D[i])
+            maxv = P[i] + D[i];
     }
-    cout << maxv << ' ';
+    cout << maxv - 1<< '\n';
 
 	return 0;
 }
