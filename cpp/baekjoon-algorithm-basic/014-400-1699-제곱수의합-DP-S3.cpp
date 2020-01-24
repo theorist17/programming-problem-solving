91581c8346a8547c023d971688d8f1e17df902f4
theorist <theorist17@gmail.com>
2020-01-25 00:29:54 +0900

    180 min. Refer video.

---
+++ cpp/baekjoon-algorithm-basic/014-400-1699-제곱수의합-DP-S3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> D(n+1, -1);
+    D[0] = 0;
+    D[1] = 1;
+    for (int i = 1 ; i * i <= n ; i++)
+        D[i*i] = 1;
+    
+
+    for (int i = 1; i <= n ; i++)
+    {
+        for (int j = 1; j * j <= i; j++)
+        {
+            if (D[i]==-1)
+                D[i] = D[j*j] + D[i - j*j];
+            else 
+                D[i] = min(D[i], D[j*j] + D[i - j*j]);
+            //cout << "i:" << i << " j:" << j << " " << D[i] << '\n';
+        }
+        //cout << D[i] << '\n';
+    }
+         
+    cout << D[n] << '\n';
+    
+	return 0;
+}
