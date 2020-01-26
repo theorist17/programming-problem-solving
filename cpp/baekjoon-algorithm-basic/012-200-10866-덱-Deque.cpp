444e52b63a604d8150897e823292b93b01fc4145
theorist <theorist17@gmail.com>
2020-01-26 21:54:16 +0900

    30 min. Implement deque.

--- cpp/baekjoon-algorithm-basic/012-200-10866-덱-Deque.cpp
+++ cpp/baekjoon-algorithm-basic/012-200-10866-덱-Deque.cpp
@@ -2,93 +2,100 @@
 #include<string>
 using namespace std;
 
-const int MAX = 1e5;
+const int N = 1e5;
 
-// class Deque
-// {
-// private:
-// 	int d[MAX], f, r;
-// public:
-// 	Deque() { f = -1; r = 0; }
-// 	void push_front(int n){
-// 		d[++f] = n;
-// 	}
-// 	void push_back(int n){
-// 		r = (r+1) % MAX;
-// 		d[r] = n;
-// 	}
-// 	int pop_front() {
-// 		if (empty())
-// 			return -1;
-// 		int ret = d[f++];
-// 		f = (f + 1) % MAX;
-// 		return ret;
-// 	}
-// 	int pop_back() {
-// 		if (empty())
-// 			return -1;
-// 		int ret = d[f++];
-// 		r = (r - 1 + MAX) % MAX;
-// 		return ret;
-// 	}
-// 	int size() {
-// 		return (r - f + MAX) % MAX;
-// 	}
-// 	bool empty() {
-// 		if (f == r)
-// 			return true;
-// 		else 
-// 			return false;
-// 	}
-// 	int front() {
-// 		if (empty())
-// 			return -1;
-// 		return d[(f+1)%MAX];
-// 	}
-// 	int back() {
-// 		if (empty())
-// 			return -1;
-// 		return d[r];
-// 	}
-// };
+class Deque
+{
+private:
+	int a[N], f, r, s;
+public:
+	Deque() { f = - 1; r = 0; s = 0;}
+	void push_front(int n){
+		if (full()) return;
+		if (f == -1) f = 0, r = 0;
+		else f = (f - 1 + N) % N;
+		a[f] = n;
+		s++;
+	}
+	void push_back(int n){
+		if (full()) return;
+		if (f == -1) f = 0, r = 0;
+		else r = (r + 1 + N) % N;
+		a[r] = n;
+		s++;
+	}
+	int pop_front() {
+		if (empty()) return -1;
+		int ret = front();
+		if (f == r) f = -1, r = 0;
+		else f = (f + 1 + N) % N;
+		s--;
+		return ret;
+	}
+	int pop_back() {
+		if (empty()) return -1;
+		int ret = back();
+		if (f == r) f = -1, r = 0;
+		else r = (r - 1 + N) % N;
+		s--;
+		return ret;
+	}
+	int size() {
+		return s;
+	}	
+	bool full() {
+		return (f == r + 1 || (f == 0 && r == N-1));
+	}
+	bool empty() {
+		return f == -1;
+	}
+	int front() {
+		if (empty()) return -1;
+		return a[f];
+	}
+	int back() {
+		if (empty()) return -1;
+		return a[r];
+	}
+};
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	// Deque s;
+	Deque s;
 
-	// int N;
-	// cin >> N;
+	int N;
+	cin >> N;
 
-	// for (int i = 0 ; i < N ; i++){
-	// 	string str;
-	// 	cin >> str;
+	for (int i = 0 ; i < N ; i++){
+		string str;
+		cin >> str;
 		
-	// 	if (!str.compare("push_back")){
-	// 		int op;
-	// 		cin >> op;
-	// 		cin.ignore();
-	// 		s.push_back(op);
-	// 	} else if (!str.compare("push_front")){
-	// 		int op;
-	// 		cin >> op;
-	// 		cin.ignore();
-	// 		s.push_front(op);
-	// 	} else if (!str.compare("pop_front")){
-	// 		cout << s.pop_front() << '\n';
-	// 	} else if (!str.compare("pop_back")){
-	// 		cout << s.pop_back() << '\n';
-	// 	} else if (!str.compare("empty")){
-	// 		cout << s.empty() << '\n';
-	// 	} else if (!str.compare("size")){
-	// 		cout << s.size() << '\n';
-	// 	} else if (!str.compare("front")){
-	// 		cout << s.front() << '\n';
-	// 	} else if (!str.compare("back")){
-	// 		cout << s.back() << '\n';
-	// 	} 
-	// }
+		if (str == "push_back"){
+			int op;
+			cin >> op;
+			s.push_back(op);
+		} else if (str == "push_front"){
+			int op;
+			cin >> op;
+			s.push_front(op);
+		} else if (str == "pop_front")
+			cout << s.pop_front() << '\n';
+		else if (str == "pop_back")
+			cout << s.pop_back() << '\n';
+		else if (str == "empty")
+			cout << s.empty() << '\n';
+		else if (str == "size")
+			cout << s.size() << '\n';
+		else if (str == "front")
+			cout << s.front() << '\n';
+		else if (str == "back")
+			cout << s.back() << '\n';
+		else if (str == "full") 
+			cout << s.back() << '\n';
+	}
+
 	return 0;
 }
