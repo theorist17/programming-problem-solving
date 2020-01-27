2f4ffe0af469810bbaddf9c2e16ee542247104c7
theorist <theorist17@gmail.com>
2020-01-27 14:57:24 +0900

    60 min.

--- cpp/snippet/015-BF-pick-C.hpp
+++ cpp/snippet/015-BF-pick-C.hpp
@@ -2,12 +2,12 @@
 #include<vector>
 using namespace std;
 
-
+void printVec(const vector<int>& vec );
 
 void pick(vector<int>& arr, vector<int>& ret, int pickedIdx, int toPick)
 {
     if (toPick == 0){
-        printVec(ret);
+        //printVec(ret);
         return;
     }
 
@@ -17,4 +17,12 @@ void pick(vector<int>& arr, vector<int>& ret, int pickedIdx, int toPick)
         pick(arr, ret, i, toPick - 1);
         ret.pop_back();
     }
+}
+
+
+void printVec(const vector<int>& vec )
+{
+    for (int i = 0 ; i < vec.size(); i++) 
+        cout << vec[i] << ' '; 
+    cout << '\n';
 }
