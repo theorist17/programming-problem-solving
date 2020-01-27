d659b3cdd57a222233b2219a50fc02d0e3cd4f13
theorist <theorist17@gmail.com>
2020-01-27 22:08:31 +0900

    30 min.

---
+++ cpp/baekjoon-algorithm-basic/015-500-1748-수이어쓰기1-BF-S3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() 
+{
+    int n, t, m = 0;
+    cin >> n;
+    t = n;
+
+    while(t)
+    {
+        m++;
+        t /= 10;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < m - 1 ; i++)
+    {
+        ans += (i+1)*(pow(10, i+1) - pow(10, i));
+        //cout << ans << "\n";
+    }
+
+    int a = n - pow(10, m - 1) + 1;
+    //cout << a << " ";
+    ans += a * m;
+
+    cout << ans << "\n";
+    
+    return 0;
+}
