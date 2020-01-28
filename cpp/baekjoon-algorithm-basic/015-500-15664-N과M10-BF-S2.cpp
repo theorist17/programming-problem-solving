40991d0faca18324f23ed928a7ea254d31ebff22
theorist17 <theorist17@gmail.com>
2020-01-28 22:09:02 +0900

    7 min.

---
+++ cpp/baekjoon-algorithm-basic/015-500-15664-N과M10-BF-S2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+vector<int> data;
+int used[9] = {0, };
+
+void pick(vector<int> &picked, int start, int length){
+    if (length == 0){
+        for(int i = 0 ; i < picked.size(); i++)
+            cout << picked[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start + 1; i <= n; i++){
+        if (!used[i]){
+            used[i] = true; picked.push_back(data[i]);
+            pick(picked, i, length-1);
+            used[i] = false; picked.pop_back();
+            while(data[i+1]==data[i]&&i<n)
+                i++;
+        }
+    }
+}
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> n >> m;
+
+    data.push_back(-1);
+    for (int i = 1 ; i <= n; i++)
+    {
+        int t; cin >> t;
+        data.push_back(t);
+    }
+
+    sort(data.begin(), data.end());
+
+    vector<int>picked;
+    pick(picked, 0, m);
+
+    return 0;
+}
