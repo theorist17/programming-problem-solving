590f3f3b49e34470514090fc15531eccde3766a4
theorist17 <theorist17@gmail.com>
2020-03-08 01:32:31 +0900

    30 min.

--- cpp/baekjoon-algorithm-basic/015-600-1707-이분그래프-Graph-S3-20m.cpp
+++ cpp/baekjoon-algorithm-basic/015-600-1707-이분그래프-Graph-S3-20m.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 int n, m;
 vector<int> g[20001];
-int check[200001] = {0, };
-int component = 0;
+int check[200001] = {0, }; // 0 , 1, 2
 
 void dfs(int node, int set) {
     check[node] = set;
-    cout << "visiting " << node << "\n";
+    // cout << "visiting " << node << "\n";
     for(int i = 0 ; i < g[node].size(); i++){
         int next = g[node][i];
-        if(check[next] != 0)
+        if(check[next] == 0)
             dfs(next, 3 - set);
     }
 }
@@ -27,7 +26,7 @@ int main() {
 
         cin >> n >> m;
 
-        for (int i = 0 ; i < n; i++) {
+        for (int i = 1 ; i <= n; i++) {
             vector<int> t;
             g[i].swap(t);
             check[i] = 0;
@@ -40,11 +39,27 @@ int main() {
             g[to].push_back(from);
         }
 
-        dfs(1, 1);
+        for(int i = 1 ; i <= n ; i++){
+            if (check[i] == 0)
+                dfs(i, 1);
+        }
+
+        bool ok = true;
+        for (int i = 1 ; i <= n ; i++) {
+            for (int j = 0; j < g[i].size(); j++) {
+                if (check[i] == check[g[i][j]]){
+                    ok = false;
+                    break;
+                }
+            }
+        }
+
+        if (ok)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+
     }
-
-    cout << component << "\n";
-
 
     return 0;
 }
